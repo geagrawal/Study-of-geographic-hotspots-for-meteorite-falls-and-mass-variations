fb41cf3e8683abaadb9de25fead61f64a3c7f433
geagrawal <149557261+geagrawal@users.noreply.github.com>
2024-09-19 12:53:01 -0500

    Add files via upload

---
+++ meteriote_main.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 22 21:53:07 2023
+
+@author: deepa
+"""
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+# If you're using Basemap, you need to install it with `conda install basemap` or `pip install basemap`
+
+!import cartopy.crs as ccrs
+!import cartopy.feature as cfeature
+
+if __name__ == "__main__":
+    
+    
+    df = pd.read_csv("C:/Users/deepa/Downloads/Meteorite_Landings (1).csv")
+    # Initialize a Cartopy figure with PlateCarree projection
+    fig, ax = plt.subplots(figsize=(12, 8), subplot_kw={'projection': ccrs.PlateCarree()})
+
+    # Add features for context
+    ax.add_feature(cfeature.COASTLINE)
+    ax.add_feature(cfeature.BORDERS, linestyle=':')
+
+    # Plot the meteorite locations on the map
+    ax.scatter(df['reclong'], df['reclat'], s=10, color='red', marker='o', alpha=0.75)
+    # Set the extent of the map if needed
+    ax.set_extent([-180, 180, -90, 90])
+
+    # Show the plot
+    plt.show()
+
+
+    
+    
+
+    
+
+
+
+
